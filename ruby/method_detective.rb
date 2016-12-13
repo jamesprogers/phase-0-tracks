be9b9ff5e4701c981e5077bde230b1be041fe2bc
James Rogers <james.p.rogers@gmail.com>
2016-12-13 10:52:30 -0500

    Case closed! - Each method research complete.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -23,17 +23,23 @@ p " suspects".insert(0, "the usual")
 # => "the usual suspects"
 
 p "The case of the disappearing last letter".chomp("r")
+p "The case of the disappearing last letter".replace("The case of the disappearing last lette")
 # => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
+p "The mystery of the missing first letter".replace("he mystery of the missing first letter")
+p "The mystery of the missing first letter".delete("T")
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+p "Elementary,    my   dear        Watson!".replace("Elementary, my dear Watson!")
+p "Elementary,    my   dear        Watson!".squeeze(" ")
+
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+p "z".replace("122")
+p "z".ord
 # => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+p "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
