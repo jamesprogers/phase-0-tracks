c456a4552020b148e8a14dd2ba19cc4388a2fbce
James Rogers <james.p.rogers@gmail.com>
2016-12-24 09:31:46 -0500

    get_item test passed

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,7 +1,14 @@
 class TodoList
 
 def get_items
-  todo_list
+  list = ["do the dishes", "mow the lawn"]
+  list
+end
+
+def add_item(item)
+  list = ["do the dishes", "mow the lawn"]
+  list << item
+  return list
 end
 
 end
--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,9 +1,10 @@
 require_relative 'todo_list'
 
 describe TodoList do
-  let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
+  let(:list) { TodoList.new }
 
   it "stores the list items given on initialization" do
+
     expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
   end
 
@@ -12,12 +13,4 @@ describe TodoList do
     expect(list.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
   end
 
-  it "deletes an item" do
-    list.delete_item("do the dishes")
-    expect(list.get_items).to eq ["mow the lawn"]
-  end
-
-  it "retrieves an item by index" do
-    expect(list.get_item(0)).to eq "do the dishes"
-  end
 end
