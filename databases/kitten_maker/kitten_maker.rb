9b2db8bf7f18272d84ec61cdf0eeaae6a9221d0b
James Rogers <james.p.rogers@gmail.com>
2017-01-15 14:53:50 -0700

    created method that generates kittens.db

--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -8,7 +8,6 @@ require 'faker'
 
 # create SQLite3 database
 db = SQLite3::Database.new("kittens.db")
-db.results_as_hash = true
 
 # learn about fancy string delimiters
 create_table_cmd = <<-SQL
@@ -22,11 +21,13 @@ SQL
 # create a kittens table (if it's not there already)
 db.execute(create_table_cmd)
 
+# add to line 11: db.results_as_hash = true
+
 # add a test kitten
 # db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
 
 # add LOOOOTS of kittens!
-# so. many. kittens. 
+# so. many. kittens.
 #KittenExplosion
 def create_kitten(db, name, age)
   db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
