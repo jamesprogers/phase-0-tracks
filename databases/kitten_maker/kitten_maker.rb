9303e68ac2a02b6f99ecf861e1375cb6bc07514b
James Rogers <james.p.rogers@gmail.com>
2017-01-16 12:09:34 -0500

    followed along video

--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -8,6 +8,7 @@ require 'faker'
 
 # create SQLite3 database
 db = SQLite3::Database.new("kittens.db")
+db.results_as_hash = true
 
 # learn about fancy string delimiters
 create_table_cmd = <<-SQL
@@ -21,10 +22,17 @@ SQL
 # create a kittens table (if it's not there already)
 db.execute(create_table_cmd)
 
-# add to line 11: db.results_as_hash = true
-
 # add a test kitten
 # db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
+
+# explore ORM by retrieving data
+kittens = db.execute("SELECT * FROM kittens")
+#puts kittens.class
+#p kittens
+
+#kittens.each do |kitten|
+#  puts "#{kitten['name']} is #{kitten['age']}"
+#end
 
 # add LOOOOTS of kittens!
 # so. many. kittens.
@@ -36,10 +44,3 @@ end
 10000.times do
   create_kitten(db, Faker::Name.name, 0)
 end
-
-# explore ORM by retrieving data
-# kittens = db.execute("SELECT * FROM kittens")
-# kittens.each do |kitten|
-#  puts "#{kitten['name']} is #{kitten['age']}"
-# end
-
